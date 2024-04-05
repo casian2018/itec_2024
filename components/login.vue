c3c4ddeffd4af633ea477436c7fa6979fbd388d1
PetricaZPC <petricazahapschiz@gmail.com>
2024-04-05 19:12:04 +0300

    update

--- components/login.vue
+++ components/login.vue
@@ -7,8 +7,8 @@
   <div class="flex flex-col bg-white shadow-md px-4 sm:px-6 md:px-8 lg:px-10 py-8 rounded-md w-full max-w-md">
     <div class="font-medium self-center text-xl sm:text-2xl uppercase text-gray-800">Login To Your Account</div>
     <button class="relative mt-6 border rounded-md py-2 text-sm text-gray-800 bg-gray-100 hover:bg-gray-200">
-      <span class="absolute left-0 top-0 flex items-center justify-center h-full w-10 text-blue-500"><i class="fab fa-facebook-f"></i></span>
-      <span>Login with Facebook</span>
+      <span class="absolute left-0 top-0 flex items-center justify-center h-full w-10 text-blue-500"><img src="https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png" class="w-8 h-8"></span>
+      <span @submit.prevent="GoogleAuthProvider">Login with Google</span>
     </button>
     <div class="relative mt-10 h-px bg-gray-300">
       <div class="absolute left-0 top-0 flex justify-center w-full -mt-2">
@@ -16,7 +16,7 @@
       </div>
     </div>
     <div class="mt-10">
-      <form action="#">
+      <form @submit.prevent="login">
         <div class="flex flex-col mb-6">
           <label for="email" class="mb-1 text-xs sm:text-sm tracking-wide text-gray-600">E-Mail Address:</label>
           <div class="relative">
@@ -26,7 +26,7 @@
               </svg>
             </div>
 
-            <input id="email" type="email" name="email" class="text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400" placeholder="E-Mail Address" />
+            <input id="email" type="email" name="email" class="text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400" placeholder="E-Mail Address" v-model="email"/>
           </div>
         </div>
         <div class="flex flex-col mb-6">
@@ -40,18 +40,18 @@
               </span>
             </div>
 
-            <input id="password" type="password" name="password" class="text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400" placeholder="Password" />
+            <input id="password" type="password" name="password" class="text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400" placeholder="Password" v-model="password"/>
           </div>
         </div>
 
         <div class="flex items-center mb-6 -mt-4">
           <div class="flex ml-auto">
-            <a href="#" class="inline-flex text-xs sm:text-sm text-blue-500 hover:text-blue-700">Forgot Your Password?</a>
+            <a href="#" class="inline-flex text-xs sm:text-sm text-blue-800 hover:text-blue-700" >Forgot Your Password?</a>
           </div>
         </div>
 
         <div class="flex w-full">
-          <button type="submit" class="flex items-center justify-center focus:outline-none text-white text-sm sm:text-base bg-blue-600 hover:bg-blue-700 rounded py-2 w-full transition duration-150 ease-in">
+          <button type="submit" class="flex items-center justify-center focus:outline-none text-white text-sm sm:text-base bg-indigo-900 hover:bg-blue-700 rounded py-2 w-full transition duration-150 ease-in">
             <span class="mr-2 uppercase">Login</span>
             <span>
               <svg class="h-6 w-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
@@ -63,7 +63,7 @@
       </form>
     </div>
     <div class="flex justify-center items-center mt-6">
-      <a href="#" target="_blank" class="inline-flex items-center font-bold text-blue-500 hover:text-blue-700 text-xs text-center">
+      <a href="#" target="_blank" class="inline-flex items-center font-bold text-blue-800 hover:text-blue-700 text-xs text-center">
         <span>
           <svg class="h-6 w-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
             <path d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z" />
@@ -78,5 +78,83 @@
 </template>
 
 
+
 <script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDTXWIiULSBECNDYj8D6U3pio0TTjyNuCc",
+    authDomain: "itec2024.firebaseapp.com",
+    projectId: "itec2024",
+    storageBucket: "itec2024.appspot.com",
+    messagingSenderId: "654897749335",
+    appId: "1:654897749335:web:86ad18e39b11b730a1b212",
+    measurementId: "G-V38WHRYJ7R"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  import { GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js";
+
+const provider = new GoogleAuthProvider();
+
+
+
+  // Import Firebase Authentication
+import {
+    getAuth,
+    signInWithEmailAndPassword
+  } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js";
+
+  // Get a reference to the Firebase Authentication service
+  const auth = getAuth(app);
+
+
+  import {signInWithPopup} from "https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js";
+
+signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    // IdP data available using getAdditionalUserInfo(result)
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
+
+  export default {
+    data() {
+      return {
+        email: '',
+        password: ''
+      }
+    },
+    methods: {
+      async login() {
+        try {
+          await signInWithEmailAndPassword(auth, this.email, this.password)
+          // Redirect to the dashboard page or perform any other action after successful login
+          this.$router.push('dash')
+        } catch (error) {
+          // Handle the error
+          console.error(error)
+        } 
+      }
+    }
+  }
 </script>
