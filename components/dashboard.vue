17631fd64fac86d68a18929a22d94f6751f6ca6c
PetricaZPC <petricazahapschiz@gmail.com>
2024-04-06 16:35:20 +0300

    updt

--- components/dashboard.vue
+++ components/dashboard.vue
@@ -87,14 +87,14 @@
       <!-- ./Sidebar -->
       <div class="h-full ml-14 mb-10 md:ml-64">
         <!-- Social Traffic -->
-        <div class="relative flex flex-col min-w-0 mb-4 lg:mb-0 break-words w-full">
-          <div class="rounded-t mb-0 px-0 border-0">
-            <div class="flex flex-wrap items-center px-4 py-2"></div>
+        <div class="relative flex flex-col min-w-0 mb-4 lg:mb-0 break-words w-full ">
+          <div class="rounded-t mb-0 px-0 border-0 ">
+            <div class="flex flex-wrap items-center px-4 py-2 "></div>
           </div>
-          <div class="dashboard">
+          <div class="dashboard ">
             <main v-if="endpointuri && endpointuri.length">
-              <div v-for="endpoint in endpointuri" :key="endpoint.id" class="endpoint-item grid grid-cols-3">
-                <p class="endpoint-url">{{ endpoint.url }}</p>
+              <div v-for="endpoint in endpointuri" :key="endpoint.id" class="endpoint-item grid grid-cols-3 mt-6">
+                <p class="endpoint-url p-1">{{ endpoint.url }}</p>
                 <p class="endpoint-status">
                   Status: {{ endpoint.status }}
                   <span class="ml-2 text-xs font-medium rounded-full px-1"></span>
@@ -227,10 +227,12 @@ export default {
 
 .navbar-title {
   margin: 0;
+  
 }
 
 .endpoint-list {
   margin-top: 20px;
+  
 }
 
 .endpoint-item {
