3eea5f16680ff52c1e026757bd08ad4dea8c8c64
CasianTheProgrammator <99408730+casian2018@users.noreply.github.com>
2024-04-06 15:48:50 +0300

    update

--- components/dashboard.vue
+++ components/dashboard.vue
@@ -1,31 +1,19 @@
 <template>
   <!-- component -->
-
   <div x-data="setup()" :class="{ 'dark': isDark }">
-    <div
-      class="min-h-screen flex flex-col flex-auto flex-shrink-0 antialiased bg-white dark:bg-gray-700 text-black dark:text-white">
-
+    <div class="min-h-screen flex flex-col flex-auto flex-shrink-0 antialiased bg-white dark:bg-gray-700 text-black dark:text-white">
       <!-- Header -->
       <div class="fixed w-full flex items-center justify-between h-14 text-white z-10">
-        <div
-          class="flex items-center justify-start md:justify-center pl-3 w-14 md:w-64 h-14 bg-blue-800 dark:bg-gray-800 border-none">
-
+        <div class="flex items-center justify-start md:justify-center pl-3 w-14 md:w-64 h-14 bg-blue-800 dark:bg-gray-800 border-none">
           <span class="hidden md:block">{{ name }}</span>
         </div>
         <div class="flex justify-between items-center h-14 bg-blue-800 dark:bg-gray-800 header-right">
-
-          <ul class="flex items-center">
-
-
-
-          </ul>
+          <ul class="flex items-center"></ul>
         </div>
       </div>
       <!-- ./Header -->
-
       <!-- Sidebar -->
-      <div
-        class="fixed flex flex-col top-14 left-0 w-14 hover:w-64 md:w-64 bg-blue-900 dark:bg-gray-900 h-full text-white transition-all duration-300 border-none z-10 sidebar">
+      <div class="fixed flex flex-col top-14 left-0 w-14 hover:w-64 md:w-64 bg-blue-900 dark:bg-gray-900 h-full text-white transition-all duration-300 border-none z-10 sidebar">
         <div class="overflow-y-auto overflow-x-hidden flex flex-col justify-between flex-grow">
           <ul class="flex flex-col py-4 space-y-1">
             <li class="px-5 hidden md:block">
@@ -34,276 +22,63 @@
               </div>
             </li>
             <li>
-              <a href="#"
-                class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-blue-800 dark:hover:bg-gray-600 text-white-600 hover:text-white-800 border-l-4 border-transparent hover:border-blue-500 dark:hover:border-gray-800 pr-6">
+              <a href="#" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-blue-800 dark:hover:bg-gray-600 text-white-600 hover:text-white-800 border-l-4 border-transparent hover:border-blue-500 dark:hover:border-gray-800 pr-6">
                 <span class="inline-flex justify-center items-center ml-4">
-                  <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                      d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6">
-                    </path>
+                  <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path>
                   </svg>
                 </span>
                 <span class="ml-2 text-sm tracking-wide truncate">Dashboard</span>
               </a>
             </li>
-            <li>
-              <a href="#"
-                class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-blue-800 dark:hover:bg-gray-600 text-white-600 hover:text-white-800 border-l-4 border-transparent hover:border-blue-500 dark:hover:border-gray-800 pr-6">
-                <span class="inline-flex justify-center items-center ml-4">
-                  <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                      d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4">
-                    </path>
-                  </svg>
-                </span>
-                <span class="ml-2 text-sm tracking-wide truncate">Board</span>
-                <span
-                  class="hidden md:block px-2 py-0.5 ml-auto text-xs font-medium tracking-wide text-blue-500 bg-indigo-50 rounded-full">New</span>
-              </a>
-            </li>
-            <li>
-              <a href="#"
-                class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-blue-800 dark:hover:bg-gray-600 text-white-600 hover:text-white-800 border-l-4 border-transparent hover:border-blue-500 dark:hover:border-gray-800 pr-6">
-                <span class="inline-flex justify-center items-center ml-4">
-                  <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                      d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z">
-                    </path>
-                  </svg>
-                </span>
-                <span class="ml-2 text-sm tracking-wide truncate">Messages</span>
-              </a>
-            </li>
-            <li>
-              <a href="#"
-                class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-blue-800 dark:hover:bg-gray-600 text-white-600 hover:text-white-800 border-l-4 border-transparent hover:border-blue-500 dark:hover:border-gray-800 pr-6">
-                <span class="inline-flex justify-center items-center ml-4">
-                  <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                      d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9">
-                    </path>
-                  </svg>
-                </span>
-                <span class="ml-2 text-sm tracking-wide truncate">Notifications</span>
-                <span
-                  class="hidden md:block px-2 py-0.5 ml-auto text-xs font-medium tracking-wide text-red-500 bg-red-50 rounded-full">1.2k</span>
-              </a>
-            </li>
-            <li class="px-5 hidden md:block">
-              <div class="flex flex-row items-center mt-5 h-8">
-                <div class="text-sm font-light tracking-wide text-gray-400 uppercase">Settings</div>
-              </div>
-            </li>
-            <li>
-              <a href="#"
-                class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-blue-800 dark:hover:bg-gray-600 text-white-600 hover:text-white-800 border-l-4 border-transparent hover:border-blue-500 dark:hover:border-gray-800 pr-6">
-                <span class="inline-flex justify-center items-center ml-4">
-                  <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                      d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
-                  </svg>
-                </span>
-                <span class="ml-2 text-sm tracking-wide truncate">Profile</span>
-              </a>
-            </li>
-            <li>
-              <a href="#"
-                class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-blue-800 dark:hover:bg-gray-600 text-white-600 hover:text-white-800 border-l-4 border-transparent hover:border-blue-500 dark:hover:border-gray-800 pr-6">
-                <span class="inline-flex justify-center items-center ml-4">
-                  <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                      d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z">
-                    </path>
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
-                  </svg>
-                </span>
-                <span class="ml-2 text-sm tracking-wide truncate">Settings</span>
-              </a>
-            </li>
+            <!-- Other sidebar items -->
           </ul>
-
         </div>
       </div>
       <!-- ./Sidebar -->
-
       <div class="h-full ml-14 mb-10 md:ml-64">
-
-
-
-
-
         <!-- Social Traffic -->
-        <div class="relative flex flex-col min-w-0 mb-4 lg:mb-0 break-words  w-full  ">
-          <div class="rounded-t mb-0 px-0 border-0 ">
-            <div class="flex flex-wrap items-center px-4 py-2">
-
-
-            </div>
-
+        <div class="relative flex flex-col min-w-0 mb-4 lg:mb-0 break-words w-full">
+          <div class="rounded-t mb-0 px-0 border-0">
+            <div class="flex flex-wrap items-center px-4 py-2"></div>
           </div>
-
-          <div class="dashboard ">
-
-            <div class="endpoint-item grid grid-cols-3 ">
-              <p class="endpoint-url ">{{ endpoint.url }}</p>
-              <p class="endpoint-status ">
-                Status: {{ endpoint.status }}
-                <span class="ml-2 text-xs font-medium rounded-full px-1" :class="`bg-${endpoint.status}-500`">
-                  {{ endpoint.status }}
-                </span>
-              </p>
-              <div class="w-full bg-gray-200 rounded ">
-                <div :class="`bg-${color}-500 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded`"
-                  :style="{ width: `${endpoint.statusWidth}%` }">
-                  {{ endpoint.statusWidth }}%
+          <div class="dashboard">
+            <main v-if="endpointuri && endpointuri.length">
+              <div v-for="endpoint in endpointuri" :key="endpoint.id" class="endpoint-item grid grid-cols-3">
+                <p class="endpoint-url">{{ endpoint.url }}</p>
+                <p class="endpoint-status">
+                  Status: {{ endpoint.status }}
+                  <span class="ml-2 text-xs font-medium rounded-full px-1" :class="`bg-${endpoint.status}-500`">{{ endpoint.status }}</span>
+                </p>
+                <div class="w-full bg-gray-200 rounded">
+                  <div :class="`bg-${color}-500 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded`" :style="{ width: `${endpoint.statusWidth}%` }">
+                    {{ endpoint.statusWidth }}%
+                  </div>
                 </div>
               </div>
-            </div>
-          </div>
-
-
-
-          <!-- ./Social Traffic -->
-
-          <!-- Recent Activities -->
-
-          <!-- ./Recent Activities -->
-        </div>
-        <!-- Statistics Cards -->
-        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 p-4 gap-4">
-          <div
-            class="bg-blue-500 dark:bg-gray-800 shadow-lg rounded-md flex items-center justify-between p-3 border-b-4 border-blue-600 dark:border-gray-600 text-white font-medium group">
-            <div
-              class="flex justify-center items-center w-14 h-14 bg-white rounded-full transition-all duration-300 transform group-hover:rotate-12">
-              <svg width="30" height="30" fill="none" viewBox="0 0 24 24" stroke="currentColor"
-                class="stroke-current text-blue-800 dark:text-gray-800 transform transition-transform duration-500 ease-in-out">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                  d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z">
-                </path>
-              </svg>
-            </div>
-            <div class="text-right">
-              <p class="text-2xl">1,257</p>
-              <p>Visitors</p>
-            </div>
-          </div>
-          <div
-            class="bg-blue-500 dark:bg-gray-800 shadow-lg rounded-md flex items-center justify-between p-3 border-b-4 border-blue-600 dark:border-gray-600 text-white font-medium group">
-            <div
-              class="flex justify-center items-center w-14 h-14 bg-white rounded-full transition-all duration-300 transform group-hover:rotate-12">
-              <svg width="30" height="30" fill="none" viewBox="0 0 24 24" stroke="currentColor"
-                class="stroke-current text-blue-800 dark:text-gray-800 transform transition-transform duration-500 ease-in-out">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                  d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"></path>
-              </svg>
-            </div>
-            <div class="text-right">
-              <p class="text-2xl">557</p>
-              <p>Orders</p>
-            </div>
-          </div>
-          <div
-            class="bg-blue-500 dark:bg-gray-800 shadow-lg rounded-md flex items-center justify-between p-3 border-b-4 border-blue-600 dark:border-gray-600 text-white font-medium group">
-            <div
-              class="flex justify-center items-center w-14 h-14 bg-white rounded-full transition-all duration-300 transform group-hover:rotate-12">
-              <svg width="30" height="30" fill="none" viewBox="0 0 24 24" stroke="currentColor"
-                class="stroke-current text-blue-800 dark:text-gray-800 transform transition-transform duration-500 ease-in-out">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                  d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"></path>
-              </svg>
-            </div>
-            <div class="text-right">
-              <p class="text-2xl">$11,257</p>
-              <p>Sales</p>
-            </div>
-          </div>
-          <div
-            class="bg-blue-500 dark:bg-gray-800 shadow-lg rounded-md flex items-center justify-between p-3 border-b-4 border-blue-600 dark:border-gray-600 text-white font-medium group">
-            <div
-              class="flex justify-center items-center w-14 h-14 bg-white rounded-full transition-all duration-300 transform group-hover:rotate-12">
-              <svg width="30" height="30" fill="none" viewBox="0 0 24 24" stroke="currentColor"
-                class="stroke-current text-blue-800 dark:text-gray-800 transform transition-transform duration-500 ease-in-out">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                  d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z">
-                </path>
-              </svg>
-            </div>
-            <div class="text-right">
-              <p class="text-2xl">$75,257</p>
-              <p>Balances</p>
-            </div>
+            </main>
+            <main v-else>
+              <p>No endpoint data available.</p>
+            </main>
           </div>
         </div>
-        <!-- ./Statistics Cards -->
-
-
-
-
-
-
-
-
       </div>
     </div>
   </div>
-
-
 </template>
 
-<style scoped>
-.dashboard {
-  padding: 20px;
-}
-
-.navbar {
-  background-color: #2196F3;
-  color: #fff;
-  padding: 10px 20px;
-}
-
-.navbar-title {
-  margin: 0;
-}
-
-.endpoint-list {
-  margin-top: 20px;
-}
-
-.endpoint-item {
-  background-color: #000000;
-  border-radius: 5px;
-  padding: 10px;
-  margin-bottom: 10px;
-}
-
-.endpoint-url {
-  margin: 0;
-}
-
-.endpoint-status {
-  margin: 5px 0;
-}
-</style>
-
-<script type="module">
+<script>
 import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-firestore.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDTXWIiULSBECNDYj8D6U3pio0TTjyNuCc",
-  authDomain: "itec2024.firebaseapp.com",
-  projectId: "itec2024",
-  storageBucket: "itec2024.appspot.com",
-  messagingSenderId: "654897749335",
-  appId: "1:654897749335:web:86ad18e39b11b730a1b212",
-  measurementId: "G-V38WHRYJ7R"
+    authDomain: "itec2024.firebaseapp.com",
+    projectId: "itec2024",
+    storageBucket: "itec2024.appspot.com",
+    messagingSenderId: "654897749335",
+    appId: "1:654897749335:web:86ad18e39b11b730a1b212",
+    measurementId: "G-V38WHRYJ7R"
 };
 
 const states = {
@@ -368,8 +143,6 @@ export default {
       }
     };
 
-    // Implement updateCallHistory function to update call history after receiving a response
-
     const getUser = async () => {
       try {
         const userData = await getDocs(collection(db, "users"));
@@ -391,5 +164,39 @@ export default {
     setInterval(fetchEndpoints, 1000);
   },
 };
-
 </script>
+
+<style scoped>
+.dashboard {
+  padding: 20px;
+}
+
+.navbar {
+  background-color: #2196F3;
+  color: #fff;
+  padding: 10px 20px;
+}
+
+.navbar-title {
+  margin: 0;
+}
+
+.endpoint-list {
+  margin-top: 20px;
+}
+
+.endpoint-item {
+  background-color: #000000;
+  border-radius: 5px;
+  padding: 10px;
+  margin-bottom: 10px;
+}
+
+.endpoint-url {
+  margin: 0;
+}
+
+.endpoint-status {
+  margin: 5px 0;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,3 @@
 <template>
 <Nav />
-<Dashboard />
 </template>
